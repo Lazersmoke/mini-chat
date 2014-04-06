cc862b6f9a310e67e3d5e8f5c0ceb46cb60fa1da
Sam Quinn <techycat1157@yahoo.com>
2014-04-06 18:55:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,36 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+
+var httpServer = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+httpServer.listen(PORT, function() {
+    console.log((new Date()) + ' Server is listening on port '+PORT);
+});
+
+wsServer = new WebSocketServer({
+    httpServer: httpServer,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+wsServer.on('request', function(request) {
+
+    var connection = request.accept('soap', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    //send history
+    connection.sendUTF(JSON.stringify({type:'history',data:history}))
+    
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            
+        }
+    })
+})
